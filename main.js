66ca049d041daa62daad319ebdd82c18f16b593e
mohitdhaundiyal <mohit.dhaundiyal22@gmail.com>
2022-01-12 21:32:31 +0530

    organize function implemented - so far so good

--- main.js
+++ main.js
@@ -5,12 +5,19 @@ let path = require("path");
 let inputArray = process.argv.slice(2);
 let command = inputArray[0];
 
+let types = {
+    media: ["mp4", "mkv", "mp3"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
+}
+
 switch (command) {
     case "help":
         help();
         break;
     case "organize":
-        organize();
+        organize(inputArray[1]);
         break;
     case "tree":
         tree(inputArray[1]);
@@ -19,7 +26,74 @@ switch (command) {
         break;
 }
 
-function organize() {
+function organize(src) {
+    let destPath;
+    // 1. take the source directory as input
+    if (src == undefined) {
+        src = process.cwd(); // if directory path is not defined - current working directory will be the default one
+    }
+
+    // 2. if src is valid we create an organized_file folder in that directory
+    if (fs.existsSync(src)) {
+        destPath = path.join(src, "organize_files");
+        if (fs.existsSync(destPath) == false) {
+            fs.mkdirSync(destPath);
+        }
+        organizeHelper(src, destPath);
+    } else {
+        console.log("please specify the correct path");
+    }
+}
+
+function organizeHelper(source, destination) {
+    // 3. identify category of each file
+    let children = fs.readdirSync(source);
+    // console.log(children);
+    for (let i = 0; i < children.length; i++) {
+        // get extention of each file - 
+        let childAdress = path.join(source, children[i]);
+        // check if childAddress is file or folder
+        // console.log(childAdress)
+        let isFile = fs.lstatSync(childAdress).isFile();
+        if (isFile) {
+            let category = getExtention(childAdress[i]);
+            // console.log(category);
+            sendFile(childAdress, destination, category);
+        }
+        // console.log(children[i]);
+    }
+}
+
+function getExtention(name) {
+    let ext = path.extname(name);
+    ext = ext.slice(1);
+    // console.log(ext);
+    for (let type in types) {
+        let typeArray = types[type];
+        for (let i = 0; i < typeArray.length; i++) {
+            if (ext == typeArray[i]) {
+                return type;
+            }
+        }
+        return "others";
+    }
+}
+
+function sendFile(fileAddress, destinationOfFile, category) {
+    // 4. send file to respective category folder
+    // create category path
+    let categoryPath = path.join(destinationOfFile, category);
+    if (fs.existsSync(categoryPath) == false) {
+        fs.mkdirSync(categoryPath);
+    }
+
+    // note : in order of copy/cut file - first you need to create duplicate file in destination folder.
+    let fileName = path.basename(fileAddress);
+    let destFilePath = path.join(categoryPath, fileName);
+    fs.copyFileSync(fileAddress, destFilePath);
+
+    // fs.unlinkSync(srcFilePath); // to cut the file
+    console.log(fileName, "copied to --------> ", category);
 }
 
 function tree(src) {
